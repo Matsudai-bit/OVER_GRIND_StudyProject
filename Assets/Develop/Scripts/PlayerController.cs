8b465fa078315cc48e778d8c746ed8c9cddc3478
YagiRyo1 <it242265@trident.ac.jp>
2026-06-08 12:27:55 +0900

    [clean]インスペクタの整理とコメント追加

--- Assets/Develop/Scripts/PlayerController.cs
+++ Assets/Develop/Scripts/PlayerController.cs
@@ -5,10 +5,16 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 20f;
-    public int maxAmmo = 10;
-    public int currentAmmo = 0;
+    [ SerializeField ]
+    GameObject bulletPrefab;
+    [ SerializeField ]
+    float bulletSpeed = 20f;
+
+    [ Header("Ammo Settings")]
+    [SerializeField]
+    int maxAmmo = 10;
+    [SerializeField]
+    int currentAmmo = 0;
 
 
     void Start()
@@ -19,7 +25,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // スペースキーで発射
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             Shoot();
@@ -28,17 +34,22 @@ public class PlayerController : MonoBehaviour
 
     void Shoot()
     {
+        // 最大弾数打っている場合は発射しない
         if (currentAmmo == maxAmmo)
         {
             return;
         }
 
+        // マウスの位置を取得
         Vector2 mousePos = Mouse.current.position.ReadValue();
 
+        // カメラからマウス位置へのRayを作成
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
+        // 発射目標位置
         Vector3 targetPoint;
 
+        // 衝突予測点を取得
         if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
         {
             targetPoint = hit.point;
@@ -48,14 +59,17 @@ public class PlayerController : MonoBehaviour
             targetPoint = ray.GetPoint(1000f);
         }
 
+        // プレイヤーから目標位置への方向を計算
         Vector3 direction = (targetPoint - transform.position).normalized;
 
+        // 弾を生成して発射
         GameObject bullet = Instantiate(
             bulletPrefab,
             transform.position,
             Quaternion.LookRotation(direction)
         );
 
+        // 弾に速度を与える
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.linearVelocity = direction * bulletSpeed;
 
