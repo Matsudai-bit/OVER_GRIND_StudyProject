9521d28454b6df88b8ba5ee5abf72618d983b339
Masaya Ishida <it242025@trident.ac.jp>
2026-06-08 12:20:22 +0900

    [add]障害物に動きのスクリプトを入れプレハブ化、空オブジェクトにジェネレータを作成

--- Assets/Develop/Scripts/GenetrateObstacle.cs
+++ Assets/Develop/Scripts/GenetrateObstacle.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class Genetrate : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,6 +12,6 @@ public class Genetrate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int rand = Random.Range(0, 10);
     }
 }
