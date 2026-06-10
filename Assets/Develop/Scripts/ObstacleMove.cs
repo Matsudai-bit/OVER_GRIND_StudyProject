32945209511491f230e625ebd95b34f83cc2cb55
Masaya Ishida <it242025@trident.ac.jp>
2026-06-10 14:38:17 +0900

    [update]ObstacleMoveの改良(弾が当たったら消えるように)

--- Assets/Develop/Scripts/ObstacleMove.cs
+++ Assets/Develop/Scripts/ObstacleMove.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ObstMove : MonoBehaviour
 {
     public float speed = 5f;
+  
 
     void Update()
     {
@@ -10,7 +11,17 @@ public class ObstMove : MonoBehaviour
         transform.Translate(Vector3.down * speed * Time.deltaTime);
 
         // 画面外（下）に出たら削除
-        if (transform.position.y < -10f)
+        if (transform.position.y < -10f )
             Destroy(gameObject);
+    }
+
+    // 衝突した瞬間
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Debug.Log("障害物に当たった！");
+            Destroy(gameObject);
+        }
     }
 }
