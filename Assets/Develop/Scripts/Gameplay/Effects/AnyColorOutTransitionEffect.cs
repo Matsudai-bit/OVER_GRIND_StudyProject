5f071db50f670504b263ee3077e99534b9f2b84f
松下大暉 <85777234+Matsudai-bit@users.noreply.github.com>
2026-06-10 10:23:15 +0900

    [add]開発環境の構築
    Development/Scripts
    以下のフォルダの追加

---
+++ Assets/Develop/Scripts/Gameplay/Effects/AnyColorOutTransitionEffect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// 色フェードシーン遷移エフェクト
+/// </summary>
+public class AnyColorOutTransitionEffect : SceneTransitionEffect
+{
+    [Header("色")]
+    [SerializeField]
+    private Color m_color;
+
+    [Header("フェード時間")]
+    [SerializeField]
+    private float m_fadeDuration = 2.0f;
+
+    [Header("白飛びイメージ")]
+    [SerializeField]
+    Image m_whiteImage;
+
+    bool m_isTransitioning = false;
+    public override bool IsTransitioning()
+    {
+        return m_isTransitioning;
+    }
+
+    public override void StartTransition(Action onComplete)
+    {
+        m_isTransitioning = true;
+
+        // 白飛びエフェクトの開始
+        m_whiteImage.gameObject.SetActive(true);
+
+        // 色の設定
+        m_whiteImage.color = new Color(m_color.r, m_color.g, m_color.b, 0);
+
+        // フェードアウトアニメーションの開始
+        m_whiteImage.DOFade( 1.1f, m_fadeDuration).SetEase(Ease.OutSine).OnComplete(() =>
+        {
+            // エフェクト完了時にコールバックを呼び出す
+            onComplete?.Invoke();
+
+            m_isTransitioning = false;
+
+        });
+
+
+
+    }
+}
