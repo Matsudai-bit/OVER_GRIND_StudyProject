beda92195435f99f9ed4f47d770ec6c4abab6826
HAYATEl011008AOKI <it242004@trident.ac.jp>
2026-06-26 09:48:15 +0900

    [Update]UIManagerの追記
    スペースを押すとHP減少
    Gを押すとGold増加
    Mを押すとMPの減少
    
    MVPからMVRPへ

---
+++ Assets/Develop/Scripts/UI/PlayerHealthModel.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UniRx;
+
+//プレイヤーのダメージ通知イベント
+public class PlayerHealthModel
+{
+    public ReactiveProperty<int> CurrentHp { get; private set; }　//体力
+    public ReactiveProperty<int> CurrentMp { get; private set; }　//魔力
+    public ReactiveProperty<int> Gold { get; private set; }　//カネ
+
+    //挿入
+    public PlayerHealthModel(int maxHp, int maxMp, int gold)
+    {
+        CurrentHp = new ReactiveProperty<int>(maxHp);   //体力
+        CurrentMp = new ReactiveProperty<int>(maxMp);   //魔力
+        Gold = new ReactiveProperty<int>(gold);    //カネ
+    }
+
+
+
+    //与えたダメージを減らす
+    public void TakeDamage(int damage)
+    {
+        CurrentHp.Value -= damage;
+        CurrentHp.Value = Mathf.Max(0, CurrentHp.Value);
+
+    }
+
+    //魔力を使用した場合減らす
+    public void UseMagic(int mpCost)
+    {
+        CurrentMp.Value = Mathf.Max(0, CurrentMp.Value - mpCost);
+    }
+
+    //カネをためる
+    public void AddGold(int amount)
+    {
+        Gold.Value += amount;
+    }
+
+}
