d87da50007e83453d68e4696f2e158a1a13dcfe8
松下大暉 <85777234+Matsudai-bit@users.noreply.github.com>
2026-07-28 00:05:09 +0900

    [add]プレイヤーの制御ファイルの追加

---
+++ Assets/PreDevelop/Daiki/Player/Scripts/PlayerStateMachineComponent.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのステートマシーンコンポーネント
+/// </summary>
+public class PlayerStateMachineComponent : MonoBehaviour
+{
+    StateMachine<PlayerStateMachineComponent> m_stateMachine;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
