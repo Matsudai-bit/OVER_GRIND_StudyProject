6ddc21b3c14dcef51336159909ec012394ef6618
松下大暉 <85777234+Matsudai-bit@users.noreply.github.com>
2026-08-13 01:47:46 +0900

    一旦実験終了

---
+++ Assets/PreDevelop/Daiki/TestNavMeshAgent.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestNavMeshAgent : MonoBehaviour
+{
+    // 目的地となるGameObjectをセットします。
+    public Transform target;
+    private NavMeshAgent myAgent;
+
+    void Start()
+    {
+        // Nav Mesh Agent を取得します。
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // targetに向かって移動します。
+        myAgent.SetDestination(target.position);
+    }
+}
