4e2742c3731acf462b08ec92f82cb1ecea705894
Riku Sasaki <oso830950@gmail.com>
2026-09-10 00:19:07 +0900

    [add]カーソルアニメーションの追加
    実装していただいたカーソルアニメーションをこちらに持ってきました

--- Assets/PreDevelop/Rikuuu/Prot_Title/Scripts/TitleButtonManager.cs
+++ Assets/PreDevelop/Rikuuu/Prot_Title/Scripts/TitleButtonManager.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class TitleButtonManager : MonoBehaviour
 {
+    Vector3 pos = Vector3.zero;
     public void MoveCursor(
-        UnityEngine.UI.Image cursor,    // カーソル
-        Vector3     targetPosition)     // 目標座標
+        UnityEngine.UI.Image cursor,             // カーソル
+        Vector3 targetPosition)     // 目標座標
     {
-        
+        if (pos == targetPosition)
+        {
+            cursor.DOFade(endValue: 1.0f, duration: 0.2f);
+        }
+        else if (pos != targetPosition)
+        {
+            cursor.DOFade(endValue: 0.0f, duration: 0.01f);
+            pos = targetPosition;
+        }
+
+        cursor.transform.DOMove(targetPosition, 0.2f);
+
     }
 }
